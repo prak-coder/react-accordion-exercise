4dca13f92f26490cad8bc91133cd35777a96b728
Prakash K <prakash.change@gmail.com>
2023-10-20 11:40:04 +0530

    accordion exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />;
+    </div>
+  );
+}
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((faq, i) => (
+        <Accordionitem
+          title={faq.title}
+          number={i + 1}
+          text={faq.text}
+          key={faq.title}
+        />
+      ))}
     </div>
   );
 }
 
-export default App;
+function Accordionitem({ title, number, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function ToggleOpen() {
+    setIsOpen((isOpen) => {
+      return !isOpen;
+    });
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={ToggleOpen}>
+      <p className="number">0{number}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen ? <p className="content-box">{text}</p> : null}
+    </div>
+  );
+}
